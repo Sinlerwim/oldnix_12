538cce398086c3849840f8302689af68dff1ebad
Kyrylo Savin <40326342+Sinlerwim@users.noreply.github.com>
2022-02-09 16:01:43 +0200

    Update setantenv.bat

--- hw_1_compilation/Ant/setantenv.bat
+++ hw_1_compilation/Ant/setantenv.bat
@@ -1,6 +1,6 @@
 @echo off
 set ANT_OPTS=-Xmx2G -Dfile.encoding=UTF-8
-set ANT_HOME=%~dp0apache-ant
+set ANT_HOME=%~dp0Ant
 set PATH=%ANT_HOME%\bin;%PATH%
 rem deleting CLASSPATH as a workaround for PLA-8702
 set CLASSPATH=
