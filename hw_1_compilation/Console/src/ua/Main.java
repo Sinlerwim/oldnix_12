391160ad953f27dfef93f53e573758ab2843fdd9
egorfuntusov <funtushan@gmail.com>
2022-02-20 13:06:51 +0200

    add review

--- hw_1_compilation/Console/src/ua/Main.java
+++ hw_1_compilation/Console/src/ua/Main.java
@@ -1,16 +1,18 @@
 package ua;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 import ua.FirstClass.FirstClass;
 import ua.SecondClass.SecondClass;
 import ua.ThirdClass.ThirdClass;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("\n\n\nWelcome to homework #1!\n\nThere are 3 classes with 2 libs in this project. ");
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System .in))) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             String line = "0";
             while (!line.isEmpty()) {
                 System.out.println("To check the name of each one and the name of lib included, input number of class you need (1-3)");
@@ -22,8 +24,9 @@ public class Main {
             System.out.println("Error: " + e);
         }
     }
+
     public static void Logic(String input) {
-        switch(input) {
+        switch (input) {
             case "1" -> System.out.println("\nThe name of class: " + new FirstClass().getClass().getName() + " Imported lib: commons-math3-3.6.1\n");
             case "2" -> System.out.println("\nThe name of class: " + new SecondClass().getClass().getName() + '\n');
             case "3" -> System.out.println("\nThe name of class: " + new ThirdClass().getClass().getName() + " Imported lib: commons-lang3-3.12.0\n");
